484923e1b09398d4e2ab387731d10352c26735d6
HenriqueCamacho <harrycamachofarias@hotmail.com>
2022-11-06 18:02:47 -0400

    Added POST, PUT, PATCH and DELETE

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response #When Django calls the api view, it expect a standard Response object
-
+from rest_framework import status
+from profiles_api import serializers
 
 # Create your views here.
 
 class HelloApiView(APIView):
     """Test Api View"""
-     
+    serializer_class = serializers.HelloSerializer
+
     #Get Method
     def get(self, request, format=None):
         """Return a list of APIView features"""
@@ -18,3 +20,32 @@ class HelloApiView(APIView):
         'Is mapped manually to URLs'
        ] 
         return Response({'message': 'Hello!', 'an_apiview':an_apiview})
+
+    #Post Method
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data = request.data)
+
+        #Validate the data
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST)  
+
+    #Put Method 
+    def put(self, request, pk=None):#Specific Primary Key
+        """Handle updating an object"""
+        return Response({'method':'PUT'})
+
+    #Patch Method
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object, like just some fields of the object"""
+        return Response({'method':'PATCH'})
+
+    #Delete Method                     
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method':'delete'})
